c43e4e8778726c8893cdb8fa949eeba4a234298d
Sreejith V S <sreejithvs333@gmail.com>
2020-10-03 15:59:06 +0530

    update comment

--- template.js
+++ template.js
@@ -1,4 +1,4 @@
-// help to switch the navigation bar states - open & close
+// help to switch the navigation-bar states - open & close
 function toggleNavigationState() {
     var x = document.getElementById("navigation");
     if (x.className === "navigation-bar") {
